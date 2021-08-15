2ee7eb954f5e8cd31c30dae492c4f2e8ac952887
guz86 <guzmo6@rambler.ru>
2021-08-16 02:11:03 +0300

    fix

--- ReplaceWithAlphabetPosition/Program.cs
+++ ReplaceWithAlphabetPosition/Program.cs
@@ -9,7 +9,7 @@
             Kata.AlphabetPosition("The sunset sets at twelve o' clock.")
             Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 */
-using System.Text;
+
 using System;
 
 namespace ReplaceWithAlphabetPosition
