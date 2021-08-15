5dce5e972739f36bc90fe8b25b51bd16193185e3
guz86 <guzmo6@rambler.ru>
2021-08-16 00:35:29 +0300

    solution

--- ReplaceWithAlphabetPosition/Program.cs
+++ ReplaceWithAlphabetPosition/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+            In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+            If anything in the text isn't a letter, ignore it and don't return it.
+
+            "a" = 1, "b" = 2, etc.
+
+            Example
+            Kata.AlphabetPosition("The sunset sets at twelve o' clock.")
+            Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+*/
+
+using System;
 
 namespace ReplaceWithAlphabetPosition
 {
@@ -6,7 +18,29 @@ namespace ReplaceWithAlphabetPosition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock.")) ;
+        }
+        public static string AlphabetPosition(string text)
+        {
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            string result = "";
+
+            for (int j = 0; j < text.Length; j++)
+            {
+
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (Char.ToLower(text[j]) == letters[i])
+                    {
+                        result += " " + (i + 1);
+                    }
+                }
+
+            }
+            result = result.Substring(1);
+            return result;
         }
     }
 }
