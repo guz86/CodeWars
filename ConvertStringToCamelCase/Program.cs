3a569af6f141c5e2cb1453d35a8a124310f8970a
guz86 <guzmo6@rambler.ru>
2021-08-16 03:01:31 +0300

    ConvertStringToCamelCase init

---
+++ ConvertStringToCamelCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+            Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+            Examples
+            "the-stealth-warrior" gets converted to "theStealthWarrior"
+            "The_Stealth_Warrior" gets converted to "TheStealthWarrior" 
+*/
+
+namespace ConvertStringToCamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToCamelCase("the-stealth-warrior"));
+            Console.WriteLine(ToCamelCase("The_Stealth_Warrior"));
+        }
+        public static string ToCamelCase(string str)
+        {
+            return str;
+        }
+    }
+}
