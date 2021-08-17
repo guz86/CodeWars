00b5e3d3dd1026dc619ded8569b1ff626b026584
guz86 <guzmo6@rambler.ru>
2021-08-17 23:10:41 +0300

    init

---
+++ FindTheMissingLetter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindTheMissingLetter
+{
+    class Program
+    {
+        public static char FindMissingLetter(char[] array)
+        {
+
+            return ' ';
+        }
+        static void Main(string[] args)
+        {
+            FindMissingLetter(new[] { 'a', 'b', 'c', 'd', 'f' });
+        }
+    }
+}
