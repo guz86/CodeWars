f1a09be75e42250dcc55af89af4c32e5c11b8442
guz86 <guzmo6@rambler.ru>
2021-08-19 00:10:34 +0300

    solution

--- BinaryAddition/Program.cs
+++ BinaryAddition/Program.cs
@@ -4,14 +4,23 @@ namespace BinaryAddition
 {
     class Program
     {
+                //Implement a function that adds two numbers together and returns their sum in binary.The conversion can be done before, or after the addition.
+                //The binary number returned should be a string.
+                //Examples:
+                //add_binary(1, 1) == "10" (1 + 1 = 2 in decimal or 10 in binary)
+                //add_binary(5, 9) == "1110" (5 + 9 = 14 in decimal or 1110 in binary)
         public static string AddBinary(int a, int b)
         {
-            return null;
+            int value = a + b;
+            string binary = Convert.ToString(value, 2);
+            return binary;
+
+            // 
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(AddBinary(2,5));
+            Console.WriteLine(AddBinary(5,9));
         }
 
     }
