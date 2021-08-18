44d14d8164af0c1a801e11eade904d80c54a2635
guz86 <guzmo6@rambler.ru>
2021-08-19 00:06:43 +0300

    init

---
+++ BinaryAddition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryAddition
+{
+    class Program
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary(2,5));
+        }
+
+    }
+}
