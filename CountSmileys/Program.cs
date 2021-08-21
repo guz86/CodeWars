4fb3149a1b67bde6e649cad540f82b3b61de2d96
guz86 <guzmo6@rambler.ru>
2021-08-22 01:07:09 +0300

    init

---
+++ CountSmileys/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CountSmileys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] smileys =  { ":D", ":~)", ";~D", ":)" };
+            CountSmileys(smileys);
+        }
+
+        public static int CountSmileys(string[] smileys)
+        {
+            return 0;
+  }
+
+    }
+}
