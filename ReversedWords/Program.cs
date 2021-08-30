2b8d308102d1e819767cc5691c06499bd7d53dc1
guz86 <guzmo6@rambler.ru>
2021-08-30 15:21:38 +0300

    init

---
+++ ReversedWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReversedWords
+{
+    class Program
+    {
+            // Complete the solution so that it reverses all of the words within the string passed in.
+            //Example:
+            //"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+        static void Main(string[] args)
+        {
+            ReverseWords("yoda doesn't speak like this");
+        }
+        public static string ReverseWords(string str)
+        {
+
+
+            return "";
+        }
+    }
+}
