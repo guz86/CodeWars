fe81aef84187932d04109b5b80f385597b1346fd
Pavel Gordienko <guzmo6@rambler.ru>
2022-09-16 06:52:02 +0300

    add 8kyu

---
+++ AbbreviateATwoWordName/AbbreviateATwoWordName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbbreviateATwoWordName
+{
+    /*
+     Write a function to convert a name into initials.
+    This kata strictly takes two words with one space in between them.
+    The output should be two capital letters with a dot separating them.
+    It should look like this:
+    Sam Harris => S.H
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "patrick feeney";
+            Console.WriteLine(AbbrevName(name));
+        }
+        
+        public static string AbbrevName(string name)
+        {
+            string[] words = name.Split(" ");
+            return $"{words[0][0]}.{words[1][0]}".ToUpper();
+            
+            //return $"{name[0]}.{name[name.IndexOf(' ') + 1]}".ToUpper();
+        }
+
+    }
+}
---
+++ ReturningStrings/ReturningStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Make a function that will return a greeting statement that uses an input; 
+    your program should return, "Hello, <name> how are you doing today?".
+
+    [Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+namespace ReturningStrings;
+
+public static class Kata
+{
+    static void Main(string[] args)
+    {
+        string name = "Alexey";
+        Console.WriteLine(Greet(name));
+    }
+    
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+}
