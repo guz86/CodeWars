b94b96fdb5b94c0149bab5e373bccc90f4914ebf
Pavel Gordienko <guzmo6@rambler.ru>
2022-09-19 08:53:53 +0300

    add solutions

---
+++ CalculateAverage/CalculateAverage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculateAverage
+{
+    class Program
+    {
+        // Write a function which calculates the average of the numbers in a given list.
+        // Note: Empty arrays should return 0.
+
+        static void Main(string[] args)
+        {
+            double[] array = {17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16};
+            Console.WriteLine(FindAverage(array));
+        }
+
+        public static double FindAverage(double[] array)
+        {
+            double avarage = 0;
+            foreach (var element in array)
+            {
+                avarage += element;
+            }
+
+            if (array.Length != 0) return avarage / array.Length;
+
+            return 0;
+        }
+
+        // return array.Length == 0 ? 0 : array.Average();
+    }
+}
---
+++ SquareEveryDigit/SquareEveryDigit/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SquareEveryDigit
+{
+    class Program
+    {
+        // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+        // For example, if we run 9119 through the function, 811181 will come out, because 9 2 is 81 and 1 2 is 1.
+        // Note: The function accepts an integer and returns an integer
+        static void Main(string[] args)
+        {
+            int n = 9119;
+            Console.WriteLine(SquareDigits(n));
+        }
+        public static int SquareDigits(int n)
+        {
+            char[] arrayChars = n.ToString().ToCharArray();
+            string str = "";
+            
+            for (int i = 0; i < arrayChars.Length; i++)
+            {
+                int temp = Convert.ToInt32(arrayChars[i].ToString());
+                str += (temp * temp).ToString();
+            }
+            
+            return Convert.ToInt32(str);
+        }
+        
+        
+        // public static int SquareDigits(int n)
+        // {
+        //     string output = "";
+        //     foreach (char c in n.ToString())
+        //     {
+        //         int square = int.Parse(c.ToString());
+        //         output += (square * square);
+        //     }
+        //     return int.Parse(output);
+        // }
+        
+        // return int.Parse(String.Concat(n.ToString().Select(a => (int)Math.Pow(char.GetNumericValue(a), 2))));
+    }
+}
