6a4ac3dce976acdd04e41378b7731ba1b55c5878
Pavel Gordienko <guzmo6@rambler.ru>
2022-09-21 15:55:53 +0300

    add Solution

---
+++ IsThisAtriangle/IsThisAtriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IsThisAtriangle
+{
+    class Program
+    {
+        // Implement a function that accepts 3 integer values a, b, c.
+        // The function should return true if a triangle can be built with the sides
+        // of given length and false in any other case.
+        // (In this case, all triangles must have surface greater than 0 to be accepted).
+        
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            Console.WriteLine(IsTriangle(a, b, c));
+        }
+        // сумма длин 2х сторон больше 3ей
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if (a+b > c &&
+                a+c > b &&
+                b+c > a 
+                ) return true;
+            else return false;
+        }
+        
+        //return (a+b > c && b+c >a && a+c > b);
+        
+        // если две самых маленьких больше третей, занчит и все больше.
+        // public static bool IsTriangle(int a, int b, int c)
+        // {
+        //     var lengths = new List<int>() {a, b, c};
+        //     lengths.Sort();
+        //     return lengths[0] + lengths[1] > lengths[2];
+        // }
+    }
+}
---
+++ TidyNumber/TidyNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TidyNumber
+{
+    class Program
+    {
+        // A Tidy number is a number whose digits are in non-decreasing order.
+        // Given a number, Find if it is Tidy or not. Number passed is always Positive. Return the result as a Boolean
+        
+        static void Main(string[] args)
+        {
+            int n = 123456;
+            Console.WriteLine(TidyNumber(n));
+        }
+        public static bool TidyNumber(int n)
+        {
+            bool result = true;
+            char[] arr = n.ToString().ToCharArray();
+            
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (Convert.ToInt32(arr[i]) > Convert.ToInt32(arr[i+1]))
+                {
+                    result = false;
+                    break;
+                }
+            }
+           
+            return result; 
+        }
+        
+        // public static bool TidyNumber(int n) => $"{n}" == string.Concat($"{n}".OrderBy(c => c));
+        // public static bool TidyNumber(int n) => n.ToString().SequenceEqual(n.ToString().OrderBy(x => x));
+        
+        // public static bool TidyNumber(int n)
+        // {
+        //     return n.ToString() == String.Concat(n.ToString().OrderBy(c => c));
+        // }
+    }
+}
