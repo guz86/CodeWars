f5881aabb677381f4f14e03a53c1e51f96fa8873
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-14 22:17:41 +0300

    fix solution 1 add solution 2

--- YAP_FindMostDuplicateCharacter/Program.cs
+++ YAP_FindMostDuplicateCharacter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 /*
     Дана строка, найти самый часто встречающийся в ней символ. 
     Если несколько символов встречаются одинаково часто, то можно вывести любой.
@@ -9,10 +11,20 @@ namespace YAP_FindMostDuplicateCharacter
     {
         static void Main(string[] args)
         {
-            string str = "abababvbvvvvv"; 
+            string str = "aaaasssssssddddddddddddsssssssssssssaaaaaaa";
+            
+            //string str = Console.ReadLine();
+            // 1
             Console.WriteLine(MostDuplicateChar(str));
+            
+            // 2
+            //string answer = new String(str.Distinct().ToArray());
+            Console.WriteLine(DistinctMostDuplicateChar(str));
+            
         }
-        
+
+        // решение 1 - сложность: n в квадрате
+
         private static string MostDuplicateChar(string str)
         {
             int symbolCount = 0;
@@ -21,7 +33,7 @@ namespace YAP_FindMostDuplicateCharacter
             for (int i = 0; i < str.Length; i++)
             {
                 tmpLetterCount = 0;
-                
+
                 for (int j = 0; j < str.Length; j++)
                 {
                     if (str[i] == str[j])
@@ -29,16 +41,49 @@ namespace YAP_FindMostDuplicateCharacter
                         tmpLetterCount++;
                     }
                 }
-                
+
                 if (tmpLetterCount > symbolCount)
                 {
-                    //symbolCount = tempSymbol;
+                    symbolCount = tmpLetterCount;
                     letter = str[i].ToString();
                 }
             }
 
             return letter;
             //return  $"{symbol} {symbolCount.ToString()}";
+        }
+
+        // решение 2 - сложность: Н*к - к-количество букв
+        // вариант пробежаться через алфавит?
+        // вариант через линку с Distinct()
+
+        private static string DistinctMostDuplicateChar(string str)
+        {
+            int symbolCount = 0;
+            int tmpLetterCount;
+            string letter = "";
+            string distinctStr = new String(str.Distinct().ToArray());
+
+            foreach (var stringChar in distinctStr)
+            {
+                tmpLetterCount = 0;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (stringChar == str[j])
+                    {
+                        tmpLetterCount++;
+                    }
+                }
+
+                if (tmpLetterCount > symbolCount)
+                {
+                    symbolCount = tmpLetterCount;
+                    letter = stringChar.ToString();
+                }
+            }
+
+            return letter;
         }
     }
 }
