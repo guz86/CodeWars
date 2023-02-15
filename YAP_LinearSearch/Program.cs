de3fb9b30da4c42c6ae82e1861a5e6c726e18162
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-16 01:18:15 +0300

    add YAP_LinearSearch

---
+++ YAP_LinearSearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace YAP_LinearSearch
+{
+    class Program
+    {
+        // найти первое левое вхождение положительного числа Х или вывести -1
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LinearSearch("121", 2));
+            Console.WriteLine(LinearSearch("213", 2));
+            Console.WriteLine(LinearSearch("132", 2));
+            Console.WriteLine(LinearSearch("133f", 2));
+            Console.WriteLine(LinearSearch("2", 2));
+            Console.WriteLine(LinearSearch("", 2));
+        }
+
+        private static int LinearSearch(string elements, int element)
+        {
+            int[] arr = new int[elements.Length];
+            for (var i = 0; i < elements.Length; i++)
+            {
+                int.TryParse(elements[i].ToString(), out int temp);
+                arr[i] = temp;
+            }
+            
+            int result = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (element == arr[i] && result == -1) result = i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ YAT_BeautifulLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAT_BeautifulLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
