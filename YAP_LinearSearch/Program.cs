cca9878f5702431b52b525a32c0c0edfb8cc3ff9
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-16 16:19:01 +0300

    add ReLinearSearch add LastLinearSearch

--- YAP_LinearSearch/Program.cs
+++ YAP_LinearSearch/Program.cs
@@ -4,19 +4,37 @@ namespace YAP_LinearSearch
 {
     class Program
     {
-        // найти первое левое вхождение положительного числа Х или вывести -1
+        
         
         static void Main(string[] args)
         {
-            Console.WriteLine(LinearSearch("121", 2));
-            Console.WriteLine(LinearSearch("213", 2));
-            Console.WriteLine(LinearSearch("132", 2));
-            Console.WriteLine(LinearSearch("133f", 2));
-            Console.WriteLine(LinearSearch("2", 2));
-            Console.WriteLine(LinearSearch("", 2));
+            Console.WriteLine("LinearSearch");
+            Console.WriteLine(LinearSearch(Sequence("121"), 2));
+            Console.WriteLine(LinearSearch(Sequence("213"), 2));
+            Console.WriteLine(LinearSearch(Sequence("132"), 2));
+            Console.WriteLine(LinearSearch(Sequence("133f"), 2));
+            Console.WriteLine(LinearSearch(Sequence("2"), 2));
+            Console.WriteLine(LinearSearch(Sequence(""), 2));
+            
+            Console.WriteLine("ReLinearSearch");
+            Console.WriteLine(ReLinearSearch(Sequence("121"), 2));
+            Console.WriteLine(ReLinearSearch(Sequence("213"), 2));
+            Console.WriteLine(ReLinearSearch(Sequence("132"), 2));
+            Console.WriteLine(ReLinearSearch(Sequence("133f"), 2));
+            Console.WriteLine(ReLinearSearch(Sequence("2"), 2));
+            Console.WriteLine(ReLinearSearch(Sequence(""), 2));
+            
+            Console.WriteLine("LastLinearSearch");
+            
+            Console.WriteLine(LastLinearSearch(Sequence("1221"), 2));
+            Console.WriteLine(LastLinearSearch(Sequence("213"), 2));
+            Console.WriteLine(LastLinearSearch(Sequence("1322"), 2));
+            Console.WriteLine(LastLinearSearch(Sequence("133f"), 2));
+            Console.WriteLine(LastLinearSearch(Sequence("22"), 2));
+            Console.WriteLine(LastLinearSearch(Sequence(""), 2));
         }
 
-        private static int LinearSearch(string elements, int element)
+        private static int[] Sequence(string elements)
         {
             int[] arr = new int[elements.Length];
             for (var i = 0; i < elements.Length; i++)
@@ -24,11 +42,42 @@ namespace YAP_LinearSearch
                 int.TryParse(elements[i].ToString(), out int temp);
                 arr[i] = temp;
             }
-            
+            return arr;
+        }
+
+        // найти первое левое вхождение положительного числа Х или вывести -1
+        // 1
+        private static int LinearSearch(int[] elements, int element)
+        {
             int result = -1;
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < elements.Length; i++)
             {
-                if (element == arr[i] && result == -1) result = i;
+                if (element == elements[i] && result == -1) result = i;
+            }
+
+            return result;
+        }
+        
+        // 2 c последнего элемента
+        private static int ReLinearSearch(int[] elements, int element)
+        {
+            int result = -1;
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                if (element == elements[i]) result = i;
+            }
+
+            return result;
+        }
+        
+        
+        // найти последнее правое вхождение положительного числа Х или вывести -1
+        private static int LastLinearSearch(int[] elements, int element)
+        {
+            int result = -1;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (element == elements[i]) result = i;
             }
 
             return result;
