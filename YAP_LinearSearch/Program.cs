8e83a42289508ca6cec0b66e681114b7d82d1db7
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-17 23:00:38 +0300

    add YAT_BeautifulLine

--- YAP_LinearSearch/Program.cs
+++ YAP_LinearSearch/Program.cs
@@ -7,8 +7,6 @@ namespace YAP_LinearSearch
 {
     class Program
     {
-        
-        
         static void Main(string[] args)
         {
             Console.WriteLine("LinearSearch");
@@ -18,7 +16,7 @@ namespace YAP_LinearSearch
             Console.WriteLine(LinearSearch(Sequence("133f"), 2));
             Console.WriteLine(LinearSearch(Sequence("2"), 2));
             Console.WriteLine(LinearSearch(Sequence(""), 2));
-            
+
             Console.WriteLine("ReLinearSearch");
             Console.WriteLine(ReLinearSearch(Sequence("121"), 2));
             Console.WriteLine(ReLinearSearch(Sequence("213"), 2));
@@ -26,7 +24,7 @@ namespace YAP_LinearSearch
             Console.WriteLine(ReLinearSearch(Sequence("133f"), 2));
             Console.WriteLine(ReLinearSearch(Sequence("2"), 2));
             Console.WriteLine(ReLinearSearch(Sequence(""), 2));
-                        
+
             Console.WriteLine("EqualsLinearSearch");
             Console.WriteLine(EqualsLinearSearch(Sequence("121"), 2));
             Console.WriteLine(EqualsLinearSearch(Sequence("213"), 2));
@@ -50,18 +48,18 @@ namespace YAP_LinearSearch
             Console.WriteLine(LastLinearSearch(Sequence("133f"), 2));
             Console.WriteLine(LastLinearSearch(Sequence("22"), 2));
             Console.WriteLine(LastLinearSearch(Sequence(""), 2));
-            
+
             // реализация методов в шапе
             Console.WriteLine("реализация методов arr 3212");
             int[] arr = Sequence("3212");
             Console.WriteLine($"Индекс элемента со значением 1 равен {Array.IndexOf(arr, 1)}");
-            Console.WriteLine($"Минимальный элемент массива: индекс {Array.IndexOf( arr, arr.Min())}");
-            Console.WriteLine($"Максимальный элемент массива: индекс {Array.IndexOf( arr, arr.Max())}");
-            
-             
+            Console.WriteLine($"Минимальный элемент массива: индекс {Array.IndexOf(arr, arr.Min())}");
+            Console.WriteLine($"Максимальный элемент массива: индекс {Array.IndexOf(arr, arr.Max())}");
+
+
             Console.WriteLine("TwoMaxLinearSearch");
             TwoMaxLinearSearch(Sequence("3214")); // [4,3]
-            
+
             Console.WriteLine("MinEvenLinearSearch");
             Console.WriteLine(MinEvenLinearSearch(Sequence("124")));
             Console.WriteLine(MinEvenLinearSearch(Sequence("214")));
@@ -70,18 +68,17 @@ namespace YAP_LinearSearch
             Console.WriteLine(MinEvenLinearSearch(Sequence("44")));
             Console.WriteLine(MinEvenLinearSearch(Sequence("1")));
             Console.WriteLine(MinEvenLinearSearch(Sequence("")));
-            
+
             Console.WriteLine("ShortWordsLinearSearch");
             string[] stringArr = {"a", "bb", "vvv", "a", "d", "ss", "qq", "q"};
             Console.WriteLine(ShortWordsLinearSearch(stringArr));
-            
+
             Console.WriteLine("RLELinearSearch");
             string stringAZ = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB";
             Console.WriteLine(RLELinearSearch(stringAZ));
             Console.WriteLine(RLELinearSearch(""));
             Console.WriteLine(RLELinearSearch("AAAABBBя"));
             Console.WriteLine(RLELinearSearch("AAAABBBz"));
-            
         }
 
         private static int[] Sequence(string elements)
@@ -92,6 +89,7 @@ namespace YAP_LinearSearch
                 int.TryParse(elements[i].ToString(), out int temp);
                 arr[i] = temp;
             }
+
             return arr;
         }
 
@@ -107,7 +105,7 @@ namespace YAP_LinearSearch
 
             return result;
         }
-        
+
         // 2 позицию - c последнего элемента
         private static int ReLinearSearch(int[] elements, int element)
         {
@@ -119,7 +117,7 @@ namespace YAP_LinearSearch
 
             return result;
         }
-        
+
         // 3 позицию - по значению c последнего элемента
         private static int EqualsLinearSearch(int[] elements, int element)
         {
@@ -131,26 +129,26 @@ namespace YAP_LinearSearch
 
             return result;
         }
-        
+
         // найти минимальное значения массива
         private static int MinLinearSearch(int[] elements, int element)
         {
-            int result = - 1; 
-            
-            if (elements.Length > 0) result = elements[0]; 
-         
+            int result = -1;
+
+            if (elements.Length > 0) result = elements[0];
+
             for (int i = 1; i < elements.Length; i++)
             {
-                if (elements[i].CompareTo(result) < 0)  // if elements[i] > result
-                    result = elements[i];  // 0 -1 1 расположен ли данный экземпляр перед, после или на той же позиции в порядке сортировки
+                if (elements[i].CompareTo(result) < 0) // if elements[i] > result
+                    result = elements[
+                        i]; // 0 -1 1 расположен ли данный экземпляр перед, после или на той же позиции в порядке сортировки
                 // лучше сохранять индекс при проверке result = i и в конце выводить элемент с нужным индексом, вместо перезаписывания переменной
             }
 
             return result;
         }
 
-        
-        
+
         // найти последнее правое вхождение положительного числа Х или вывести -1
         private static int LastLinearSearch(int[] elements, int element)
         {
@@ -162,16 +160,16 @@ namespace YAP_LinearSearch
 
             return result;
         }
-        
-        
+
+
         // найти максимальное число в последовательности и второе по величине число (которое будет максимальным если вычеркнуть ОДНО первое)
         // 3 2 1 3  гарантированно минимум 2 элемента в последовательности
-        
+
         private static int[] TwoMaxLinearSearch(int[] elements)
         {
-            int max1 = elements[1] > elements[0] ? elements[1] : elements[0];   // b > a ? b : a;
+            int max1 = elements[1] > elements[0] ? elements[1] : elements[0]; // b > a ? b : a;
             int max2 = elements[1] < elements[0] ? elements[1] : elements[0];
-            
+
             for (int i = 2; i < elements.Length; i++)
             {
                 if (elements[i] > max1)
@@ -187,14 +185,14 @@ namespace YAP_LinearSearch
 
             return new[] {max1, max2};
         }
-        
+
         // Найти минимальное четное число в последовательности или вывести -1, если такого не существует
-        
+
         private static int MinEvenLinearSearch(int[] elements)
         {
             int result = -1;
             bool even = false;
-            
+
             for (int i = 0; i < elements.Length; i++)
             {
                 if (elements[i] % 2 == 0 & (!even || elements[i] < result))
@@ -203,15 +201,16 @@ namespace YAP_LinearSearch
                     even = true;
                 }
             }
+
             return result;
         }
-        
-        
+
+
         // Дана последовательность слов. Вывести все самые короткие слова через пробел
         // требуется двойной проход, чтобы это было быстрее (1. поиск минимальной длины, 2. собираем строку)
         // не нужно пересоздавать каждый раз новую строку, поэтому нужно собирать список а потом из списка делать строку
         // "a", "bb", "vvv", "a", "d", "ss", "qq", "q"
-        
+
         private static string ShortWordsLinearSearch(string[] words)
         {
             int minLen = words[0].Length;
@@ -221,7 +220,7 @@ namespace YAP_LinearSearch
                 if (word.Length < minLen) minLen = word.Length;
             }
 
-            List<string> shortWords = new List<string>(); 
+            List<string> shortWords = new List<string>();
 
             foreach (var word in words)
             {
@@ -229,61 +228,64 @@ namespace YAP_LinearSearch
             }
 
             return String.Join((" "), shortWords.ToArray());
-        }        
-        
-        // RLE способ сжатия АААА в А4
-         /*
-          Дана строка, состоящая из букв A-Z:
-          "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-          Нужно написать функцию RLE, которая на выходе даст строку вида:
-          "A4B3C2XYZD4E3F3A6B28"
-          И сгенерирует любую ошибку, если на вход пришла невалидная строка.
-         
-          Пояснение:
-          1. если символ встречается 1 раз, он остается без изменений
-          2. если символ повторяется более 1 раза, к нему добавляется количество повторений
-         */
-         
-         // - Оставить уникальные буквы, посчитать их количество
-         
-         private static string RLELinearSearch(string stringAZ)
-         {
-             if(stringAZ.Length == 0) {
-                 return "пустая строка";
-             }
-             
-             if (!Regex.IsMatch(stringAZ, "^[A-Z0-9]*$")) {
-                 return "невалидная строка";
-             }
-             
-             // if (!stringAZ.All(char.IsLetterOrDigit)) {
-             //     return "невалидная строка";
-             // }
-             
-             // if (!stringAZ.All(c => (c >= 48 && c <= 57 || c >= 97 && c <= 122))) {
-             //     return "невалидная строка";
-             // }
-             
-             string lastChar = stringAZ[0].ToString();
+        }
 
-             List<string> singleChars = new List<string>(); 
-             
-             int lastPos = 0;
-             for (int i = 0; i < stringAZ.Length; i++)
-             {
-                 if (stringAZ[i].ToString() != lastChar)
-                 {
-                     string count = (i - lastPos) > 1 ? (i - lastPos).ToString() : "";
-                     
-                     singleChars.Add(lastChar + count);
-                     lastPos = i;
-                     lastChar = stringAZ[i].ToString();
-                 }
-             }
-             singleChars.Add(lastChar + (stringAZ.Length  - lastPos));
-             
-             //return String.Join((""), singleChars.ToArray());
-             return String.Concat(singleChars);
-         }      
+        // RLE способ сжатия АААА в А4
+        /*
+         Дана строка, состоящая из букв A-Z:
+         "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+         Нужно написать функцию RLE, которая на выходе даст строку вида:
+         "A4B3C2XYZD4E3F3A6B28"
+         И сгенерирует любую ошибку, если на вход пришла невалидная строка.
+        
+         Пояснение:
+         1. если символ встречается 1 раз, он остается без изменений
+         2. если символ повторяется более 1 раза, к нему добавляется количество повторений
+        */
+
+        // - Оставить уникальные буквы, посчитать их количество
+
+        private static string RLELinearSearch(string stringAZ)
+        {
+            if (stringAZ.Length == 0)
+            {
+                return "пустая строка";
+            }
+
+            if (!Regex.IsMatch(stringAZ, "^[A-Z0-9]*$"))
+            {
+                return "невалидная строка";
+            }
+
+            // if (!stringAZ.All(char.IsLetterOrDigit)) {
+            //     return "невалидная строка";
+            // }
+
+            // if (!stringAZ.All(c => (c >= 48 && c <= 57 || c >= 97 && c <= 122))) {
+            //     return "невалидная строка";
+            // }
+
+            string lastChar = stringAZ[0].ToString();
+
+            List<string> singleChars = new List<string>();
+
+            int lastPos = 0;
+            for (int i = 0; i < stringAZ.Length; i++)
+            {
+                if (stringAZ[i].ToString() != lastChar)
+                {
+                    string count = (i - lastPos) > 1 ? (i - lastPos).ToString() : "";  // или можно сделать отдельный счетчик который увеличивать и сбрасывать
+
+                    singleChars.Add(lastChar + count);
+                    lastPos = i;
+                    lastChar = stringAZ[i].ToString();
+                }
+            }
+
+            singleChars.Add(lastChar + (stringAZ.Length - lastPos));
+
+            //return String.Join((""), singleChars.ToArray());
+            return String.Concat(singleChars);
+        }
     }
 }
--- YAT_BeautifulLine/Program.cs
+++ YAT_BeautifulLine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace YAT_BeautifulLine
 {
@@ -6,7 +7,88 @@ namespace YAT_BeautifulLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int.TryParse(Console.ReadLine(), out int count);
+            //string line = Console.ReadLine();
+
+
+            //int count = 2;
+            //string line = "abcaz";
+
+            //Console.WriteLine(BeautifulLine(count, line));
+            //  Console.WriteLine(BeautifulLine(2, "abcaz"));
+            //  Console.WriteLine(BeautifulLine(2, "helto"));
+            // Console.WriteLine(BeautifulLine(3, "abvcaz"));
+            // Console.WriteLine(BeautifulLine(4, "abvcfaz"));
+            // Console.WriteLine(BeautifulLine(4, "babvcfaz"));
+            Console.WriteLine(CharacterReplacement(4, "abcazd"));
+        }
+
+        private static int BeautifulLine(int count, string line)
+        {
+            int result = 0;
+
+            char lastChar;
+            //char findChar = ' ';
+            int countChar;
+            bool notLine = true;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                countChar = 0;
+                lastChar = line[i];
+
+                for (var j = i + 1; j < line.Length; j++)
+                {
+                    if (line[j] != lastChar)
+                    {
+                        countChar++;
+                    }
+                    else if (countChar == count)
+                    {
+                        //findChar = lastChar;
+                        result = 2 + count;
+                        notLine = false;
+                    }
+                }
+            }
+
+            //return findChar;
+            if (notLine) result = 1 + count;
+
+            return result;
+        }
+
+
+        // правильное решение
+
+        public static int CharacterReplacement( int k, string s)
+        {
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            int left = 0;
+            int result = 0;
+            int max = 0;
+            for (int right = 0; right < s.Length; right++)
+            {
+                if (!map.ContainsKey(s[right]))
+                {
+                    map.Add(s[right], 1);
+                }
+                else
+                {
+                    map[s[right]]++;
+                }
+
+                max = Math.Max(map[s[right]], max);
+                while ((right - left + 1) - max > k)
+                {
+                    map[s[left]]--;
+                    left = left + 1;
+                }
+
+                result = Math.Max(right - left + 1, result);
+            }
+
+            return result;
         }
     }
 }
